75b345ed8b41f8aa0707558ea83990d08244f794
Sergey Georgiev <sgrgv_art@mail.ru>
2023-04-20 11:50:57 +0300

    new

---
+++ src/UI/components/newsPost/NewsPost.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./NewsPost.module.css";
+
+const NewsPost = () => {
+  return <div className={classes.newsPost}>Hello world</div>;
+};
+
+export default NewsPost;
